7c2cb7ec0866f05c71eadb35f67dcfcb05cccaaa
Nathan Colgate <nathan@brandnewbox.com>
2023-12-09 06:38:48 -0600

    Day 09

---
+++ 09/a.rb
@@ -0,0 +1,21 @@
+class Sequence
+  def initialize(values)
+    @values = values
+  end
+  def deltas
+    @values.each_cons(2).map { |a,b| b-a }
+  end
+  def subsequence
+    Sequence.new(deltas)
+  end
+  def prediction
+    @values.uniq.length == 1 ? @values.first : @values.last + subsequence.prediction
+  end
+end
+
+readings = []
+input = File.read(File.expand_path("./input.txt"))
+input.split("\n").each do |string|
+  readings << Sequence.new(string.split(' ').map(&:to_i))
+end
+puts readings.map(&:prediction).sum
